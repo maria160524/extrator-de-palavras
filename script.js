ceec8777de478c6f0ab37873af710ff8c92cfee1
maria160524 <maria.laval@escola.pr.gov.br>
2025-09-16 09:32:27 -0300

    aula 4

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
 
 function mostraPalavrasChave() {
     const texto = document.querySelector("#entrada-de-texto").Value;
-
     const campoResultado = document.querySelactor("#resultado-palavrachave");
-
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto(texto);
     
-    campoResultado.textcontent = palavras;
+    campoResultado.textcontent = palavras.join(",");
+}
+
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
 }
