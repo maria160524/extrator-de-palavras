0dad7f129ada5ed7f04f489c1aa7bff2dc36a0fd
maria160524 <maria.laval@escola.pr.gov.br>
2025-09-30 10:04:21 -0300

    aula6

--- script.js
+++ script.js
@@ -12,5 +12,24 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
-    return palavras;
-}
+    const frequencias=contrarequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+
+    function ordenapalavra(p1, p2){
+        return frequencias[p2]- frequencias[p1
+{
+    console. log (ordenadas);
+    retuern ordenadas.slice(0,10);
+}      
+function contrafrequencias(palavras) {
+    let frequencias= {};
+    for (let i of palavras){
+        frequencia[i] =0;
+        for(let j of palavras){
+            if(i==j){
+                frequencias[i]++;
+            }
+        }
+    }
+        return palavras;
+    }
